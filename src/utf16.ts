446cd57ae9ba666df89b56af5a5f01612eb4f6ba
i-xi-dev <naoyuki.isono@gmail.com>
2024-01-08 22:24:34 +0900

    1.0.0

--- src/utf16.ts
+++ src/utf16.ts
@@ -1,5 +1,5 @@
 import { BOM, TextEncoderBase } from "./main.ts";
-import { StringEx } from "../deps.ts";
+import { NumberEx, StringEx } from "../deps.ts";
 
 const _BE_LABEL = "utf-16be";
 const _LE_LABEL = "utf-16le";
@@ -7,7 +7,7 @@ const _LE_LABEL = "utf-16le";
 function _encode(
   label: string,
   input: string,
-  fatal: boolean, //TODO 単独のサロゲートの場合のみ？
+  fatal: boolean, // エンコードのエラーは単独のサロゲートの場合のみ？
   prependBOM: boolean,
   replacementChar: string,
 ): Uint8Array {
@@ -22,8 +22,7 @@ function _encode(
     chars = input;
   }
 
-  const bytesPerElement = Uint16Array.BYTES_PER_ELEMENT;
-  const buffer = new ArrayBuffer(chars.length * bytesPerElement);
+  const buffer = new ArrayBuffer(chars.length * NumberEx.Uint16.BYTES);
   const view = new DataView(buffer);
   let charCount = 0;
   for (const rune of chars) {
@@ -32,17 +31,18 @@ function _encode(
         throw new TypeError("input[*]");
       } else {
         view.setUint16(
-          charCount * bytesPerElement,
+          charCount * NumberEx.Uint16.BYTES,
           replacementChar.charCodeAt(0),
           label === _LE_LABEL,
         );
         charCount = charCount + 1;
+        continue;
       }
     }
 
     for (let i = 0; i < rune.length; i++) {
       view.setUint16(
-        charCount * bytesPerElement,
+        charCount * NumberEx.Uint16.BYTES,
         rune.charCodeAt(i),
         label === _LE_LABEL,
       );
@@ -106,4 +106,32 @@ export namespace Utf16le {
     prependBOM?: boolean;
     replacementChar?: string;
   };
+
+  export class Encoder extends TextEncoderBase {
+    constructor(options: EncoderOptions = {}) {
+      super(_LE_LABEL, {
+        fatal: options?.fatal === true,
+        prependBOM: options?.prependBOM === true,
+        replacementChar: _replacementChar(options?.replacementChar),
+      });
+    }
+
+    override encode(input = ""): Uint8Array {
+      return _encode(
+        _LE_LABEL,
+        input,
+        this.fatal,
+        this.prependBOM,
+        this._replacementChar,
+      );
+    }
+
+    override encodeInto(
+      source: string,
+      destination: Uint8Array,
+    ): TextEncoderEncodeIntoResult {
+      //TODO
+      throw new Error("not implemented.");
+    }
+  }
 }
--- src/utf32.ts
+++ src/utf32.ts
@@ -1,106 +1,174 @@
-import { BOM, isString } from "./main.ts";
-import { CodePoint } from "./code_point.ts";
-import { Encoding } from "./encoding.ts";
+import { BOM, TextDecoderBase, TextEncoderBase } from "./main.ts";
+import { NumberEx, StringEx } from "../deps.ts";
 
 const _BE_LABEL = "utf-32be";
 const _LE_LABEL = "utf-32le";
 
-const _BYTES_PER_ELEMENT = Uint32Array.BYTES_PER_ELEMENT;
+// function _decode(
+//   label: string,
+//   input: BufferSource,
+//   options: Encoding.DecodeOptions,
+// ): string {
+//   let view: DataView;
+//   if (ArrayBuffer.isView(input)) {
+//     view = new DataView(input.buffer);
+//   } else if (input instanceof ArrayBuffer) {
+//     view = new DataView(input);
+//   } else {
+//     throw new TypeError("input");
+//   }
 
-function _decode(
-  label: string,
-  input: BufferSource,
-  options: Encoding.DecodeOptions,
-): string {
-  let view: DataView;
-  if (ArrayBuffer.isView(input)) {
-    view = new DataView(input.buffer);
-  } else if (input instanceof ArrayBuffer) {
-    view = new DataView(input);
-  } else {
-    throw new TypeError("input");
-  }
+//   if (view.byteLength % NumberEx.Uint32.BYTES !== 0) {
+//     throw new TypeError("input");
+//   }
 
-  if (view.byteLength % _BYTES_PER_ELEMENT !== 0) {
-    throw new TypeError("input");
-  }
+//   const runes = [];
+//   let codePoint: number;
+//   for (let i = 0; i < view.byteLength; i = i + NumberEx.Uint32.BYTES) {
+//     codePoint = view.getUint32(i, label === _LE_LABEL);
+//     if (CodePoint.isCodePoint(codePoint) !== true) {
+//       throw new TypeError("input[*]");
+//     }
+//     runes.push(
+//       String.fromCodePoint(codePoint),
+//     );
+//   }
 
-  const runes = [];
-  let codePoint: number;
-  for (let i = 0; i < view.byteLength; i = i + _BYTES_PER_ELEMENT) {
-    codePoint = view.getUint32(i, label === _LE_LABEL);
-    if (CodePoint.isCodePoint(codePoint) !== true) {
-      throw new TypeError("input[*]");
-    }
-    runes.push(
-      String.fromCodePoint(codePoint),
-    );
-  }
-
-  const str = runes.join("");
-  if (options?.ignoreBOM === true) {
-    return str;
-  } else {
-    return str.startsWith(BOM) ? str.substring(1) : str;
-  }
-}
+//   const str = runes.join("");
+//   if (options?.ignoreBOM === true) {
+//     return str;
+//   } else {
+//     return str.startsWith(BOM) ? str.substring(1) : str;
+//   }
+// }
 
 function _encode(
   label: string,
   input: string,
-  options: Encoding.EncodeOptions,
+  fatal: boolean, // エンコードのエラーは単独のサロゲートの場合のみ？
+  prependBOM: boolean,
+  replacementChar: string,
 ): Uint8Array {
-  if (isString(input) !== true) {
+  if (StringEx.isString(input) !== true) {
     throw new TypeError("input");
   }
 
   let runes: string[];
-  if ((options?.prependBOM === true) && (input.startsWith(BOM) !== true)) {
+  if ((prependBOM === true) && (input.startsWith(BOM) !== true)) {
     runes = [...(BOM + input)];
   } else {
     runes = [...input];
   }
 
-  const buffer = new ArrayBuffer(runes.length * _BYTES_PER_ELEMENT);
+  const buffer = new ArrayBuffer(runes.length * NumberEx.Uint32.BYTES);
   const view = new DataView(buffer);
-  for (let i = 0; i < runes.length; i++) {
+  let runeCount = 0;
+  for (const rune of runes) {
+    const codePoint = rune.codePointAt(0) as number;
+    if (StringEx.CodePoint.isSurrogateCodePoint(codePoint)) {
+      if (fatal === true) {
+        throw new TypeError("input[*]");
+      } else {
+        view.setUint32(
+          runeCount * NumberEx.Uint32.BYTES,
+          replacementChar.codePointAt(0) as number,
+          label === _LE_LABEL,
+        );
+        runeCount = runeCount + 1;
+        continue;
+      }
+    }
+
     view.setUint32(
-      i * _BYTES_PER_ELEMENT,
-      runes[i].codePointAt(0) as number,
+      runeCount * NumberEx.Uint32.BYTES,
+      codePoint,
       label === _LE_LABEL,
     );
+    runeCount = runeCount + 1;
   }
   return new Uint8Array(buffer);
 }
 
-export namespace Utf32be {
-  export function decode(
-    input: BufferSource = new Uint8Array(0),
-    options: Encoding.DecodeOptions = {},
-  ): string {
-    return _decode(_BE_LABEL, input, options);
-  }
+const _defaultReplacementChar = "\u{FFFD}";
 
-  export function encode(
-    input = "",
-    options: Encoding.EncodeOptions = {},
-  ): Uint8Array {
-    return _encode(_BE_LABEL, input, options);
+function _replacementChar(replacementChar?: string): string {
+  if (StringEx.isNonEmptyString(replacementChar) !== true) {
+    return _defaultReplacementChar;
+  }
+  if ((replacementChar as string).length !== 1) { //XXX runeもokにすべき？？
+    return _defaultReplacementChar;
+  }
+  return replacementChar as string;
+}
+
+export namespace Utf32be {
+  export type EncoderOptions = {
+    fatal?: boolean;
+    prependBOM?: boolean;
+    replacementChar?: string;
+  };
+
+  export class Encoder extends TextEncoderBase {
+    constructor(options: EncoderOptions = {}) {
+      super(_BE_LABEL, {
+        fatal: options?.fatal === true,
+        prependBOM: options?.prependBOM === true,
+        replacementChar: _replacementChar(options?.replacementChar),
+      });
+    }
+
+    override encode(input = ""): Uint8Array {
+      return _encode(
+        _BE_LABEL,
+        input,
+        this.fatal,
+        this.prependBOM,
+        this._replacementChar,
+      );
+    }
+
+    override encodeInto(
+      source: string,
+      destination: Uint8Array,
+    ): TextEncoderEncodeIntoResult {
+      //TODO
+      throw new Error("not implemented.");
+    }
   }
 }
 
 export namespace Utf32le {
-  export function decode(
-    input: BufferSource = new Uint8Array(0),
-    options: Encoding.DecodeOptions = {},
-  ): string {
-    return _decode(_LE_LABEL, input, options);
-  }
+  export type EncoderOptions = {
+    fatal?: boolean;
+    prependBOM?: boolean;
+    replacementChar?: string;
+  };
 
-  export function encode(
-    input = "",
-    options: Encoding.EncodeOptions = {},
-  ): Uint8Array {
-    return _encode(_LE_LABEL, input, options);
+  export class Encoder extends TextEncoderBase {
+    constructor(options: EncoderOptions = {}) {
+      super(_LE_LABEL, {
+        fatal: options?.fatal === true,
+        prependBOM: options?.prependBOM === true,
+        replacementChar: _replacementChar(options?.replacementChar),
+      });
+    }
+
+    override encode(input = ""): Uint8Array {
+      return _encode(
+        _LE_LABEL,
+        input,
+        this.fatal,
+        this.prependBOM,
+        this._replacementChar,
+      );
+    }
+
+    override encodeInto(
+      source: string,
+      destination: Uint8Array,
+    ): TextEncoderEncodeIntoResult {
+      //TODO
+      throw new Error("not implemented.");
+    }
   }
 }
