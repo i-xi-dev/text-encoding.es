0c0ff8f5e067379314c9594f190525534002548e
i-xi-dev <naoyuki.isono@gmail.com>
2024-06-15 23:44:45 +0900

    6.3.8

--- deps.ts
+++ deps.ts
@@ -1,7 +1,7 @@
 export {
   CodePoint,
   RuneString,
-} from "https://raw.githubusercontent.com/i-xi-dev/unicode.es/1.0.0-beta.1/mod.ts";
+} from "https://raw.githubusercontent.com/i-xi-dev/unicode.es/1.0.0-beta.2/mod.ts";
 export { StringEx } from "https://raw.githubusercontent.com/i-xi-dev/string.es/7.0.0/mod.ts";
 export { SafeIntegerFormat } from "https://raw.githubusercontent.com/i-xi-dev/number-format.es/1.0.3/mod.ts";
 export {
