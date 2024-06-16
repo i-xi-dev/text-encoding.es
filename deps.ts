f6048734bb2828e793868cfe4bf7e93d35f215f4
i-xi-dev <naoyuki.isono@gmail.com>
2024-06-17 01:53:56 +0900

    6.3.9

--- deps.ts
+++ deps.ts
@@ -1,13 +1,13 @@
 export {
   CodePoint,
   RuneString,
-} from "https://raw.githubusercontent.com/i-xi-dev/unicode.es/1.0.0-beta.2/mod.ts";
-export { StringEx } from "https://raw.githubusercontent.com/i-xi-dev/string.es/7.0.0/mod.ts";
-export { SafeIntegerFormat } from "https://raw.githubusercontent.com/i-xi-dev/number-format.es/1.0.3/mod.ts";
+} from "https://raw.githubusercontent.com/i-xi-dev/unicode.es/1.0.1/mod.ts";
+export { StringEx } from "https://raw.githubusercontent.com/i-xi-dev/string.es/7.0.1/mod.ts";
+export { SafeIntegerFormat } from "https://raw.githubusercontent.com/i-xi-dev/number-format.es/1.0.4/mod.ts";
 export {
   Radix,
   SafeInteger,
   Uint7,
   Uint8,
-} from "https://raw.githubusercontent.com/i-xi-dev/number.es/8.0.1/mod.ts";
-export { _TransformStream } from "https://raw.githubusercontent.com/i-xi-dev/compat.es/1.2.19/mod.ts";
+} from "https://raw.githubusercontent.com/i-xi-dev/number.es/8.1.0/mod.ts";
+export { _TransformStream } from "https://raw.githubusercontent.com/i-xi-dev/compat.es/1.2.20/mod.ts";
